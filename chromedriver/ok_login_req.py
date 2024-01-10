ef07ddc45150d768a454c6b34f5519732fb25a27
palas1k <liserg008@gmail.com>
2024-01-10 22:35:48 +0300

    upd

--- chromedriver/ok_login_req.py
+++ chromedriver/ok_login_req.py
@@ -12,70 +12,61 @@ load_dotenv()
 login = os.getenv("LOGIN_PHONE")
 password = os.getenv("PASSWORD")
 
-data = {
-    'st.redirect': '',
-    'st.asr': '',
-    'st.posted': 'set',
-    'st.fJS': 'on',
-    'st.st.screenSize': '1600 x 900',
-    'st.st.browserSize': '739',
-    'st.st.flashVer': '0.0.0',
-    'st.email': login,
-    'st.password': password,
-}
-
-url = "https://ok.ru/dk?cmd=AnonymLogin&st.cmd=anonymMain"
 
 
 def logged_check(func):
     async def wrapped(*args):
         resp = await func(*args)
-        if ConnectToOk().session is not None:
+        if resp is not None:
             return resp
         else:
+            print("Non logged")
             return None
 
     return wrapped
 
 
-class ConnectToOk:
+class Auth:
     session = None
+    data = {
+        'st.redirect': '',
+        'st.asr': '',
+        'st.posted': 'set',
+        'st.fJS': 'on',
+        'st.st.screenSize': '1600 x 900',
+        'st.st.browserSize': '739',
+        'st.st.flashVer': '0.0.0',
+        'st.email': login,
+        'st.password': password,
+    }
 
+    url = "https://ok.ru/dk?cmd=AnonymLogin&st.cmd=anonymMain"
+
+    @logged_check
     def start_session(self):
-        return aiohttp.ClientSession()
-
-    async def stop_session(self):
-        return await self.session.close()
-
-    # post
-    async def get_data(self):
-        self.session = await self.start_session().post(url=url, data=data)
+        self.session = aiohttp.ClientSession().post(url=self.url, data=self.data)
         return self.session
 
 
 class OkParser:
 
-    obj = ConnectToOk()
+    def __init__(self):
+        self.auth_session = Auth()
 
     async def get_data(self):
-        obj = await self.obj.get_data()
+        obj = await self.auth_session.start_session()
         return await obj.text()
 
-    async def stop_session(self):
-        await self.obj.stop_session()
 
-
-
-    @logged_check
     async def check_messages(self) -> str:
         resp_data = await self.get_data()
         soup = BeautifulSoup(resp_data, "html.parser")
         answer = soup.find(id='counter_ToolbarMessages').text
         print(f"Новых сообщений: {answer}")
-        await self.stop_session()
+        self.auth_session.start_session().close()
         # return answer
 
-    @logged_check
+
     async def check_notifications(self) -> str:
         resp_data = await self.get_data()
         soup = BeautifulSoup(resp_data, "html.parser")
@@ -84,7 +75,7 @@ class OkParser:
             print(f"Новых оповещений: {answer}")
         else:
             print("Новых оповещений нет")
-        await self.stop_session()
+        await self.auth_session.close()
         # return answer
 
 
@@ -93,14 +84,3 @@ asyncio.run(runner.check_messages())
 
 # runner = ConnectToOk()
 # asyncio.run(runner.auth_to_ok())
-
-
-@logged_check
-async def hobby_to_ok():
-    async with aiohttp.ClientSession() as session:
-        async with session.post(url='https://ok.ru/hobby') as response:
-            print(response.cookies.get("AUTHCODE"))
-            return await response.text()
-
-
-# asyncio.run(hobby_to_ok())
